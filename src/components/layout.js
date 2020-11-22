183dfaaf435bc2e60d51e1aa3b1272228e0f6748
Melanie Archer <job@twobanjos.com>
2020-11-22 15:09:36 -0800

    Update font to Piazzolla

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from '../components/header';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import 'fontsource-piazzolla/500.css';
+import 'fontsource-piazzolla/700.css';
 
 export default ({ children }) => (
 <div className="page">
