706ec2cf1c8770680b4e4db44f55587efc6a1803
Melanie Archer <job@twobanjos.com>
2020-12-17 15:44:04 -0800

    Change theme colors * change bg and fg colors * remove previous layout * add <head> to layout with Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Header from '../components/header';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
-import 'fontsource-piazzolla/500.css';
-import 'fontsource-piazzolla/700.css';
 
 export default ({ children }) => (
 <div className="page">
+  <Helmet>
+    <title>Two Banjos At Once</title>
+  </Helmet>
 <Header />
 <Nav />
 <main>
