1ebb19a60112cf88c4ae0213004521c7eec8f661
Melanie Archer <job@twobanjos.com>
2020-12-18 17:37:39 -0800

    * new Google Fonts * revised backgrounds

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,8 @@ export default ({ children }) => (
 <div className="page">
   <Helmet>
     <title>Two Banjos At Once</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,400&family=Trirong:wght@500&display=swap" rel="stylesheet" /> 
   </Helmet>
 <Header />
 <Nav />
