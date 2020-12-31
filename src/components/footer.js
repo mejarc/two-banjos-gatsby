a55aca535b3419f4daa62b2ed04b08e7786ccc92
Melanie Archer <job@twobanjos.com>
2020-12-30 17:53:44 -0800

    2021 refresh. New colors; revised portfolio copy; updated footer date.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => (
 <footer>
       <p>
-      All content &copy;1998  &#8211; 2020 M. Archer, unless otherwise indicated.
+      All content &copy;1998  &#8211; 2021 M. Archer, unless otherwise indicated.
       </p>
 </footer>
 );
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import headerStyles from './header.module.css';
 export default () => (
-<header className={headerStyles.header} role="banner">
+<header className="header" role="banner">
 <h1>Two Banjos At Once.</h1>
 </header>
 );
